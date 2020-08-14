c924c3be28e7d63ac2ee0e381af9395576cf9650
Vandana-Pv <pv.vandana29@gmail.com>
2020-08-14 17:21:18 +0530

    Initial setup and basic blueprint of instagram

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Post from './Post'
 
 function App() {
+  // Setting the State. State is a virtual memory
+  const [posts, setPosts] = useState([
+    {
+      username: "VandanaPv",
+      caption: "Hello World", 
+      imageUrl: "https://images.pexels.com/photos/1072851/pexels-photo-1072851.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    },
+    {
+      username: "Meghana", 
+      caption: "This is cool", 
+      imageUrl: "https://images.pexels.com/photos/4464377/pexels-photo-4464377.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    }
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt=""
+          />
+      </div>
+      <h1>Hey there from Instagram</h1>
+
+      {/* Posts */}
+
+      {
+        posts.map((post) =>(
+          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
+        ))
+      }
     </div>
   );
 }
